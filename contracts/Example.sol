81af355ce6e119f8c8ea4d145a8508e5b36faaa9
akshay-na <67007337+akshay-na@users.noreply.github.com>
2022-05-26 19:05:58 +0530

    Prettier run

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract Example {
-  address private _administrator;
+    address private _administrator;
 
-  constructor() {
-    // Make the deployer of the contract the administrator
-    _administrator = msg.sender;
-  }
+    constructor() {
+        // Make the deployer of the contract the administrator
+        _administrator = msg.sender;
+    }
 
-  function isAdministrator(address user) public view returns (bool) {
-    return user == _administrator;
-  }
+    function isAdministrator(address user) public view returns (bool) {
+        return user == _administrator;
+    }
 }
